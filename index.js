bf68deb8af7130ec4c0b2c3530b861a34efc5bef
Austin Robinson <austin@austinrobinson.ca>
2020-09-12 20:20:00 -0700

    Delete JS File

--- index.js
+++
@@ -1,9 +0,0 @@
-function resizeIframeWidth(e){
-    // Set width of iframe according to its content
-    if (e.Document && e.Document.body.scrollWidth) //ie5+ syntax
-        e.width = e.contentWindow.document.body.scrollWidth;
-    else if (e.contentDocument && e.contentDocument.body.scrollWidth) //ns6+ & opera syntax
-        e.width = e.contentDocument.body.scrollWidth + 35;
-    else (e.contentDocument && e.contentDocument.body.offsetWidth) //standards compliant syntax – ie8
-        e.width = e.contentDocument.body.offsetWidth + 35;
-}
